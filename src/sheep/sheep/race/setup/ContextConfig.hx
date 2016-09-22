72f2b7d83e47afe3ebb8968f72bd2eb8a005565a
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2016-09-22 03:14:11 -0300

    created ContextConfig

---
+++ src/sheep/sheep/race/setup/ContextConfig.hx
@@ -0,0 +1,43 @@
+package sheep.sheep.race.setup;
+import pixi.core.display.Container;
+import sheep.sheep.race.events.FlowEvent;
+import sheep.sheep.race.mediators.GameViewMediator;
+import sheep.sheep.race.mvc.FlowManager;
+import sheep.sheep.race.mvc.MediatorMap;
+import sheep.sheep.race.mvc.ViewManager;
+import sheep.sheep.race.mediators.HomeViewMediator;
+import sheep.sheep.race.views.GameView;
+import sheep.sheep.race.views.HomeView;
+import sheep.sheep.race.views.IntroView;
+
+/**
+ * ...
+ * @author Ronaldo Santiago
+ */
+class ContextConfig 
+{
+	var stage:Container;
+	var flowManager:FlowManager;
+	var mediatorMap:MediatorMap;
+	
+	public function new(stage:Container) 
+	{
+		this.stage = stage;
+		
+		mediatorMap = new MediatorMap();
+		mediatorMap.map(HomeView, HomeViewMediator);
+		mediatorMap.map(GameView, GameViewMediator);
+		
+		flowManager = new FlowManager( stage, mediatorMap );
+		
+		flowManager.mapSetView( FlowEvent.SHOW_HOME, HomeView );
+		flowManager.mapSetView( FlowEvent.SHOW_GAME, GameView );
+		
+		init();
+	}
+	
+	function init() 
+	{
+		stage.emit(FlowEvent.SHOW_HOME, FlowEvent.SHOW_HOME);
+	}	
+}
